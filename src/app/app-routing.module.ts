2be7a403805fb011cc49532392be05edcdb6f720
archaether <archaether@gmail.com>
2019-04-08 16:48:38 -0400

    added app routing to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RequestComponent} from './components/request/request.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'request', component: RequestComponent}
+  { path: 'request', component: RequestComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
