f56a0ba22132a52fa1c731c3dc976e4a3ad393fc
archaether <archaether@gmail.com>
2019-04-09 09:29:37 -0400

    added string validation to login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string;
+  uErrorMessage: string;
+  pErrorMessage: string;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+test(){
+  alert('hello!');
+  //$("#user_message").html('changed');
+}
+validUsername(username: string){
+  if(username == null || username == ""){
+    this.uErrorMessage="Please enter a username!";
+  }
+  else{
+    this.uErrorMessage = "";
+  }
+}
+validPassword(password: string){
+  if(password == null || password == ""){
+    this.pErrorMessage = "Please enter a password.";
+  }
+  else{
+    this.pErrorMessage = "";
+  }
+}
 }
