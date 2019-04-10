374b58bdcc856c13e3a81cec4f0020bcb38716e9
archaether <archaether@gmail.com>
2019-04-10 16:18:17 -0400

    moved window.location to != statement of login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -74,6 +74,7 @@ export class LoginComponent implements OnInit {
           console.log("User recieved!" + JSON.stringify(this.rider));
           this.loginErrMsg = '';
           RiderService.globalRider = this.rider; //must make global rider public static
+          window.location.href = "/home"; //redirects a user
         }
         else{
           console.log("User not found");
@@ -89,7 +90,7 @@ export class LoginComponent implements OnInit {
       //make sure validPassword returns otherwise evaluated as void
       this.remember();
       this.login(username, password);
-      window.location.href = "/home"; //redirects a user
+      
     }
     else {
       alert('Please fillout all forms!');
