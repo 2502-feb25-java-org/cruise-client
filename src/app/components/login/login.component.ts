dbd6f22bf5c8a59790503056740472f6d77f7e4f
archaether <archaether@gmail.com>
2019-04-10 09:22:30 -0400

    added a link to sign up page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,64 +12,62 @@ export class LoginComponent implements OnInit {
   uErrorMessage: string;
   pErrorMessage: string;
   rememberBox;
-  
 
   constructor() { }
 
   ngOnInit() {
-    if(localStorage.getItem('rememberMe') == 'true'){
+    if (localStorage.getItem('rememberMe') == 'true') {
       this.username = localStorage.getItem('storedUsername');
       this.rememberBox = localStorage.getItem('rememberMe');
     }
-    else{
+    else {
       this.username = "";
       this.rememberBox = false;
     }
   }
 
-submit(username: string, password: string){
-  if(this.validUsername(username) || this.validPassword(password)){
-    this.remember();
-    this.login();
+  submit(username: string, password: string) {
+    if (this.validUsername(username) || this.validPassword(password)) {
+      this.remember();
+      this.login();
+    }
+    else {
+      alert('Please fillout all forms!');
+    }
   }
-  else{
-    alert('Please fillout all forms!');
+  validUsername(username: string) {
+    if (username == null || username == "") {
+      this.uErrorMessage = "Please enter a username!";
+      return false;
+    }
+    else {
+      this.uErrorMessage = "";
+      return true;
+    }
   }
-  
-}
-validUsername(username: string){
-  if(username == null || username == ""){
-    this.uErrorMessage="Please enter a username!";
-    return false;
-  }
-  else{
-    this.uErrorMessage = "";
-    return true;
-  }
-}
-validPassword(password: string){
-  if(password == null || password == ""){
-    this.pErrorMessage = "Please enter a password.";
-  }
-  else{
-    this.pErrorMessage = "";
-  }
-}
-
-remember(){
-  if(this.rememberBox){
-   
-    localStorage.setItem('storedUsername', this.username);
-    localStorage.setItem('rememberMe', this.rememberBox);
-  }
-  else {
-    localStorage.setItem('rememberMe', this.rememberBox);
-    localStorage.setItem('storedUsername', "");
+  validPassword(password: string) {
+    if (password == null || password == "") {
+      this.pErrorMessage = "Please enter a password.";
+    }
+    else {
+      this.pErrorMessage = "";
+    }
   }
 
-}
+  remember() {
+    if (this.rememberBox) {
 
-login(){
+      localStorage.setItem('storedUsername', this.username);
+      localStorage.setItem('rememberMe', this.rememberBox);
+    }
+    else {
+      localStorage.setItem('rememberMe', this.rememberBox);
+      localStorage.setItem('storedUsername', "");
+    }
 
-}
+  }
+
+  login() {
+
+  }
 }
