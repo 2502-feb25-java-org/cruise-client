fa7a3703cd20a460cdadc709c1a989648a1c8163
David <goodescientist@gmail.com>
2019-04-10 10:04:30 -0400

    new stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +8,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { RequestComponent } from './components/request/request.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     RequestComponent,
     SignupComponent,
-    HomeComponent 
+    HomeComponent,
+    FooterComponent 
   ],
   imports: [
     BrowserModule,
