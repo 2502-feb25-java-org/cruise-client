96f9b92a104c42b7a222341466f1e066cab66b57
archaether <archaether@gmail.com>
2019-04-11 15:47:37 -0400

    Register button now disables if not valid. Changed URL

--- src/app/services/rider/rider.service.ts
+++ src/app/services/rider/rider.service.ts
@@ -10,7 +10,8 @@ export class RiderService {
   //==variables==
   name: string ='This is my singleton rider service';
 
-  API_URL: string = 'http://localhost:8080/cruise/rider';
+  API_URL: string = 'http://ec2-18-218-174-33.us-east-2.compute.amazonaws.com:8080/cruise/rider';
+  //
   //==user==
   public static globalRider: Rider;
  
