91ebfc4791278d939c2119293a07fa0dc3ee4941
David <goodescientist@gmail.com>
2019-04-11 16:11:57 -0400

    two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'support', component: FeedbackComponent},
   { path: 'request', component: RequestComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
