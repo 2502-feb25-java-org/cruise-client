0071445fc822198f7d27ac71517d6e19005ee3d4
archaether <archaether@gmail.com>
2019-04-12 14:07:09 -0400

    changed a few things

--- src/app/services/rider/rider.service.ts
+++ src/app/services/rider/rider.service.ts
@@ -26,16 +26,16 @@ export class RiderService {
     return 'properly injected service';
   }
 
-  public getByUsernameAndPassword(rider: Rider): Observable<Rider>{
-    console.log(this.http.post<Rider>(this.API_URL + "/get",rider));
-    return this.http.post<Rider>(this.API_URL + "/get",rider);
+  //public getByUsernameAndPassword(rider: Rider): Observable<Rider>{
+    //console.log(this.http.post<Rider>(this.API_URL + "/find",rider));
+   // return this.http.post<Rider>(this.API_URL + "/find",rider);
   // public getAll(): Observable<Rider[]> {
   //   return this.http.get<Rider[]>(this.API_URL + "/getall");
   // }
   
-  // public getByUsernameAndPassword(username: string, password: string): Observable<Rider>{
-  //   console.log("Inside RiderService getByUsernameAndPassword method");
-  //   return this.http.post<Rider>(this.API_URL + "/get", [username, password], this.httpOptions);
+  public getByUsernameAndPassword(username: string, password: string): Observable<Rider>{
+    console.log("Inside RiderService getByUsernameAndPassword method");
+    return this.http.post<Rider>(this.API_URL + "/get", [username, password]);
 }
 
 
