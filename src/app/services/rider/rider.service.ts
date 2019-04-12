3c2033d0674688312147c6fdad131b54fb56bda4
unknown <levlevit@gmail.com>
2019-04-12 15:59:17 -0400

    good

--- src/app/services/rider/rider.service.ts
+++ src/app/services/rider/rider.service.ts
@@ -29,13 +29,8 @@ export class RiderService {
   public postRider(rider: Rider): Observable<Rider>{
     return this.http.post<Rider>(this.API_URL + "/add", rider, this.httpOptions);
   }
-  // public getAll(): Observable<Rider[]> {
-  //   return this.http.get<Rider[]>(this.API_URL + "/getall");
-  // }
 
   public getByUsernameAndPassword(username: string, password: string): Observable<Rider>{
     return this.http.post<Rider>(this.API_URL + "/find", [username, password], this.httpOptions);
   }
-
-
 }
