f815a7af9eb73f9eb698f4ac2b8981138ea5623c
unknown <levlevit@gmail.com>
2019-04-16 09:59:40 -0400

    working build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
-import { MapComponent } from './components/map/map.component';
 import { EditprofileComponent } from './components/editprofile/editprofile.component'
 
 const routes: Routes = [
@@ -17,7 +16,6 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent},
   { path: 'users', component: ProfileComponent},
-  { path: 'temp', component: MapComponent},
   { path: 'editprofile', component: EditprofileComponent}
   
 ];
